92ca0804604a1cc3178e364fca54e2171c36f4f7
hmlxy <128121982+hmlxy@users.noreply.github.com>
2023-05-26 15:34:48 +0800

    1 file modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  // development更快的构建速度、更容易的调试、更有用的日志提示和错误信息
   mode: 'development',
+  // 表示编译目标在web环境运行的代码
   target: 'web',
   devServer: {
+    // 默认浏览器
     open: 'google-chrome',
+    // 默认端口
     port: 9000,
+    // 指定静态文件的根目录
     contentBase: [
       path.join(__dirname, 'src'),
       path.join(__dirname, 'dist'),
     ],
+    // 监控静态文件的变化并自动刷新网页
     watchContentBase: true,
+    // 启用浏览器自动刷新
     liveReload: true
   },
   module: {
